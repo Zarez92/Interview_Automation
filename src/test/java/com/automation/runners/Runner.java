41b78bd5d278be7010fcdf8a394c478e30b43a3f
Zarko_Nikolic <zare.92@hotmail.com>
2023-10-19 15:23:42 -0400

    Project SetUp

---
+++ src/test/java/com/automation/runners/Runner.java
@@ -0,0 +1,23 @@
+package com.automation.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/automation/steps",
+        dryRun = true,
+        tags = "",
+        publish = true //generating a report with public link
+)
+public class Runner {}
+
